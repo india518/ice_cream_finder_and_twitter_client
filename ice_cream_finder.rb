d3e50d7e48b22e3a8c61ff92603beac551b98a24
App Academy Student <student@appacademy.io>
2013-03-21 14:03:22 -0700

    Display directions to user

--- ice_cream_finder.rb
+++ ice_cream_finder.rb
@@ -1,39 +1,46 @@
 require 'nokogiri'
 require 'json'
 require 'rest-client'
+require 'addressable/uri'
+
+Places_API_Key = "AIzaSyALm2QA3GrjC3KriARJyawhJG-sw_qbLmo"
 
 def get_response(request)
-  response_str = RestClient.get(request)
+  response_str = RestClient.get(request.to_s)
   JSON.parse(response_str)
 end
-
-#Step 1, get our latitude/longitude
 
 puts "Enter your location and I'll find you good ice cream."
 # user_address = gets.chomp.gsub(' ', '+')
 user_address = "160 Folsom St San Fransicso 94105".gsub(' ', '+')
-request = "http://maps.googleapis.com/maps/api/geocode/json"
-request += "?address=#{user_address}&sensor=false"
+
+request = Addressable::URI.new(
+  :scheme => "http",
+  :host => "maps.googleapis.com",
+  :path => "/maps/api/geocode/json",
+  :query_values => {
+    :address => "#{user_address}",
+    :sensor => "false"
+  }
+)
 response = get_response(request)
-#puts JSON.pretty_generate(response)
+
 user_latlng = response['results'][0]['geometry']['location']
-
-
-
-#step 2: give our location to Places API and ask for ice cream places
-
-#Places API key:
-Places_API_Key = "AIzaSyALm2QA3GrjC3KriARJyawhJG-sw_qbLmo"
-
-places_request = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
-places_request << "?key=#{Places_API_Key}"
-places_request << "&location=#{user_latlng['lat']},#{user_latlng['lng']}"
-places_request << "&radius=500"
-places_request << "&sensor=false"
-places_request << "&keyword=ice+cream"
-places_request << "&type=food"
-
+places_request = Addressable::URI.new(
+  :scheme => "https",
+  :host => "maps.googleapis.com",
+  :path => "/maps/api/place/nearbysearch/json",
+  :query_values => {
+    :key => "#{Places_API_Key}",
+    :location => "#{user_latlng['lat']},#{user_latlng['lng']}",
+    :radius => "500",
+    :sensor => "false",
+    :keyword => "ice+cream",
+    :type => "food"
+  }
+)
 places_response = get_response(places_request)
+
 ice_cream_shops = {}
 places_response["results"].each do |result|
   ice_cream_shop_name = result["name"]
@@ -48,27 +55,29 @@ end
 
 puts "Which number shop would you like directions to?"
 choice = gets.to_i
-
-#step 3: get directions shop
 name = ice_cream_shops.keys[choice]
-
 puts "Directions to #{name}:"
-
 dest_latlng = ice_cream_shops[name]
 
-p dest_latlng
-
-directions_request = "http://maps.googleapis.com/maps/api/directions/json"
-directions_request << "?origin=#{user_latlng['lat']},#{user_latlng['lng']}"
-directions_request << "&destination=#{dest_latlng['lat']},#{dest_latlng['lng']}"
-directions_request << "&sensor=false"
-directions_request << "&mode=walking"
-
+directions_request = Addressable::URI.new(
+  :scheme => "http",
+  :host => "maps.googleapis.com",
+  :path => "/maps/api/directions/json",
+  :query_values => {
+    :origin => "#{user_latlng['lat']},#{user_latlng['lng']}",
+    :destination => "#{dest_latlng['lat']},#{dest_latlng['lng']}",
+    :radius => "500",
+    :sensor => "false",
+    :mode => "walking"
+  }
+)
 directions_result = get_response(directions_request)
 
-p directions_result
-
-
-
-
-
+# Use to pretty up interface later:
+#directions_result["routes"][0]["legs"][0]["start_address"]
+steps = directions_result["routes"][0]["legs"][0]["steps"]
+steps.each_with_index do |step, index|
+  parsed_html_instructions = Nokogiri::HTML(step["html_instructions"])
+  puts "Step #{index + 1}: #{parsed_html_instructions.text}"
+end
+#directions_result["routes"][0]["legs"][0]["end_address"]
