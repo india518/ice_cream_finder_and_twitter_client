8b001f1bc03b76739ee26745a82e1e6945468c78
App Academy Student <student@appacademy.io>
2013-03-21 15:10:15 -0700

    Add user verification with access_token

--- twitter.rb
+++ twitter.rb
@@ -1,9 +1,21 @@
 require 'launchy'
 require 'oauth'
 require 'yaml'
-require 'secrets.rb'
+require './secrets'
 
-CONSUMER_KEY = PvtoobjZOCIfNduWZujWEA
+CONSUMER = OAuth::Consumer.new(
+  CONSUMER_KEY, CONSUMER_SECRET, :site => "https://twitter.com")
+
+def request_access_token
+  request_token = CONSUMER.get_request_token
+  authorize_url = request_token.authorize_url
+  puts "Go to this URL: #{authorize_url}"
+  Launchy.open(authorize_url)
+  puts "Login, and type your verification code in"
+    oauth_verifier = gets.chomp
+  access_token = request_token.get_access_token(
+          :oauth_verifier => oauth_verifier)
+end
 
 class User
   attr_accessor :real_name, :user_name
@@ -16,8 +28,9 @@ class User
   def dm(message)
   end
 
-
-
+  def self.timeline(access_token)
+    access_token.get("http://api.twitter.com/1.1/statuses/user_timeline.json").body
+  end
 end
 
 
@@ -27,7 +40,5 @@ class Status
     @user = user
     @message = message
   end
-
-
 
 end
