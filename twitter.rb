e7ab7dc1dd39565bdda8df6e4f26c502f222a26c
App Academy Student <student@appacademy.io>
2013-03-21 17:49:36 -0700

    Update user timeline retrieval for any user

--- twitter.rb
+++ twitter.rb
@@ -1,6 +1,8 @@
 require 'launchy'
 require 'oauth'
 require 'yaml'
+require 'json'
+require 'addressable/uri'
 require './secrets'
 
 CONSUMER = OAuth::Consumer.new(
@@ -14,18 +16,29 @@ class User
   end
 
   def statuses
-    EndUser.access_token.get("http://api.twitter.com/1.1/statuses/home_timeline.json").body
+    request = Addressable::URI.new(
+      :scheme => "https",
+      :host => "api.twitter.com",
+      :path => "/1.1/statuses/user_timeline.json",
+      :query_values => {
+        :screen_name => @user_name
+      }
+    ).to_s
+    p request
+    p EndUser.access_token
+    EndUser.access_token.get(request).body
   end
 
-  def User.timeline
-    EndUser.access_token.get("http://api.twitter.com/1.1/statuses/user_timeline.json").body
-  end
 end
 
 class EndUser < User
 
   def self.access_token
     @@access_token
+  end
+
+  def self.current_user
+    @@current_user
   end
 
   def self.login(user_name)
@@ -36,10 +49,15 @@ class EndUser < User
     puts "Login, and type your verification code in"
       oauth_verifier = gets.chomp
     @@access_token = request_token.get_access_token(:oauth_verifier => oauth_verifier)
+    @@current_user = EndUser.new(user_name)
   end
 
   def dm(message)
     p access_token.post("https://api.twitter.com/1.1/direct_messages/new.json")
+  end
+
+  def timeline
+    EndUser.access_token.get("http://api.twitter.com/1.1/statuses/home_timeline.json").body
   end
 
 end
